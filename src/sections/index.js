64fa9e2cc3fe862e4b780f90b98845d27e9dbe2c
sidaswal7 <siddhantaswal7@gmail.com>
2024-09-02 21:33:00 +0530

    added hero section

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+
+export {
+    Hero,
+    PopularProducts,
+    Subscribe,
+    SpecialOffer,
+    CustomerReviews,
+    Footer,
+    Services, 
+    SuperQuality
+}
